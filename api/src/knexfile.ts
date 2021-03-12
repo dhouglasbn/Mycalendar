1044250a111398e709753418637c8430353cdf37
Dhouglas <dhouglasbn@gmail.com>
2021-03-12 10:27:33 -0300

    knexfile.ts added

---
+++ api/src/knexfile.ts
@@ -0,0 +1,13 @@
+// importando path para evitar bugs de diretórios
+// windows usa \ e macOS usa /
+import path from 'path';
+
+module.exports = {
+    client: "sqlite3", // definindo sqlite3 como o meu banco de dados
+    connection: { // criando a conexão com o meu database.sqlite
+        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+    },
+    migrations: { // criando minhas migrations na pasta migrations
+        filename: path.resolve(__dirname, 'src', 'database', 'migrations')
+    }
+}
