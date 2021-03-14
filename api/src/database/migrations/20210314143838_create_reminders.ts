96eccc3fb3857d9ef63b12e4959705f96c49300b
Dhouglas <dhouglasbn@gmail.com>
2021-03-14 11:41:26 -0300

    create_reminders added

---
+++ api/src/database/migrations/20210314143838_create_reminders.ts
@@ -0,0 +1,20 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("reminders", table => {
+        table.uuid("id").primary(),
+
+        table.integer("user_id")
+        .notNullable()
+        .references("id")
+        .inTable("users")
+
+        table.string("date").notNullable()
+    } )
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+}
+
