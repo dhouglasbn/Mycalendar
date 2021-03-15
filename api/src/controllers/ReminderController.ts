27a971c45b73a59a46b34b11c34df26b4396fbde
Dhouglas <dhouglasbn@gmail.com>
2021-03-15 11:16:22 -0300

    update 15/03

--- api/src/controllers/ReminderController.ts
+++ api/src/controllers/ReminderController.ts
@@ -19,6 +19,14 @@ class ReminderController {
         // convertendo o formato da data da requisição para UTC
         const UTCDate = new Date(date).toISOString()
 
+        // verificando se o reminder já existe
+        const dateAlreadyExists = await knex("reminders").select("*").where("date", UTCDate);
+        if(dateAlreadyExists.length >= 1) {
+            return response.status(401).json({
+                "error": "this reminder already exists!"
+            })
+        }
+
         // armazenando todos os dados que vão para o banco de dados
         const data = {
             id,
