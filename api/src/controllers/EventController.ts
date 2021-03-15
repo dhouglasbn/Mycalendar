3007e3d88927b6052cd081806366d0a5869d058b
Dhouglas <dhouglasbn@gmail.com>
2021-03-15 10:26:24 -0300

    EventController finished

---
+++ api/src/controllers/EventController.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+import { v4 as uuid } from "uuid";
+import knex from "../database/connections";
+
+
+class EventController {
+    async create(request: Request, response: Response) {
+        // gerar nossa chave uuid
+        const id = uuid();
+
+        // coletar nossos dados da requisição
+        const { title,
+            start_date, 
+            finish_date, 
+            people, 
+            location, 
+            description } = request.body;
+        
+        const email = request.headers.email;
+
+        // tentando encontrar o id do usuário do email no banco de dados
+        const user = await knex("users").select("id").where("email", email);
+        const user_id = user[0].id;
+
+        // convertendo o formato da data da requisição para UTC
+        const UTCStartDate = new Date(start_date).toISOString();
+        const UTCFinishDate = new Date(finish_date).toISOString();
+
+        // armazenando todos os dados que vão para o banco de dados
+        const data = {
+            id,
+            user_id,
+            title,
+            start_date: UTCStartDate,
+            finish_date: UTCFinishDate,
+            people,
+            location,
+            description
+        }
+
+        // inserir minha data no banco de dados
+        await knex("events").insert(data);
+
+        // retornando os dados
+        return response.json(data);
+    }
+}
+
+export { EventController }
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
+
 import { UserController } from './controllers/UserController';
+import { EventController } from "./controllers/EventController";
 
 const router = Router();
 
 const userController = new UserController;
+const eventController = new EventController;
 
 router.post("/register", userController.create); // registrar usuário
 router.get("/login", userController.logIn); // verificar e logar conta do usuário
+
+router.post("/create", eventController.create); // criar um evento
 
 export { router }
