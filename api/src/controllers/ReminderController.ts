b7a15f6127406bc6d293b3e17a53b814b9b045b2
Dhouglas <dhouglasbn@gmail.com>
2021-03-16 10:12:55 -0300

    function isbefore added to requesst

--- api/src/controllers/ReminderController.ts
+++ api/src/controllers/ReminderController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import knex from "../database/connections";
 import { v4 as uuid } from "uuid";
+import "moment";
+import moment from "moment";
 
 
 class ReminderController {
@@ -12,20 +14,22 @@ class ReminderController {
         const { title, date } = request.body;
         const email = request.headers.email;
 
-        // tentando encontrar o id do usuário do email no banco de dados
+        // tentar encontrar o id do usuário do email no banco de dados
         const user = await knex("users").select("id").where("email", email)
         const user_id = user[0].id;
 
         // convertendo o formato da data da requisição para UTC
         const UTCDate = new Date(date).toISOString()
 
-        // verificando se o reminder já existe
-        const dateAlreadyExists = await knex("reminders").select("*").where("date", UTCDate);
-        if(dateAlreadyExists.length >= 1) {
+        // verificar se a data da requisição é antecessora da data atual
+        if(moment(UTCDate).isBefore(new Date())) {
             return response.status(401).json({
-                "error": "this reminder already exists!"
+                "error": "this date is in the past"
             })
         }
+
+        // verificando se o reminder já existe
+        const dateAlreadyExists = await knex("reminders").select("*").where("date", UTCDate);
 
         // armazenando todos os dados que vão para o banco de dados
         const data = {
