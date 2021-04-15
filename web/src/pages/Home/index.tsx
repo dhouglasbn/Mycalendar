67cbd989a91d3b175bf9e54d64f221e65c66dfd4
dhouglas <dhouglasbn@gmail.com>
2021-04-15 12:50:33 -0300

    trying to change logo color

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles.css";
 
+import logo from "../../Assets/calendar.svg";
+
 const Home = () => {
     return (
         <div id="headers">
             <div id="logo">
+                <img src={logo} alt="MyCalendar"/>
                 <h1>MyCalendar</h1>
-                <h2>You're in Home</h2>
             </div>
         </div>
     )
