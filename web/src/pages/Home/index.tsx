62fc25d58472586812289f5017ce3eb67d601135
dhouglas <dhouglasbn@gmail.com>
2021-04-16 11:31:28 -0300

    Home header fixed & finished

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -5,11 +5,16 @@ import logo from "../../Assets/calendar.svg";
 
 const Home = () => {
     return (
-        <div id="headers">
-            <div id="logo">
-                <iframe title="logo" src={logo} />
-                <h1>MyCalendar</h1>
-            </div>
+        <div id="body">
+            <header >
+                <div id="logo">
+                    <iframe title="logo" src={logo} />
+                    <h1>MyCalendar</h1>
+                </div>
+            </header>
+            <main>
+                
+            </main>
         </div>
     )
 }
