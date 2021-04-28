3fb3d2622844d827c3f8eb8e2c346b3ca33b74e5
dhouglas <dhouglasbn@gmail.com>
2021-04-28 12:30:02 -0300

    handleSelected button function] added

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 import logo from "../../Assets/calendar.svg";
 
 const Home = () => {
+
+    const [signValue, setSignValue] = useState("0");
+
+    function handleSelectedButton(buttonId: string) {
+        const button = document.getElementById(buttonId)
+        
+        if(button?.className === "selected") {
+            return;
+        }
+        if(signValue === "1") {
+            setSignValue("0")
+            return;
+        }  
+        setSignValue("1");
+        return;
+    }
     return (
         <div id="home-page">
             <header id="home-page-header">
@@ -15,13 +31,26 @@ const Home = () => {
             <main id="home-page-main">
                 <form action="" method="get">
                     <div id="form-header">
-                        <button id="unselected" type="button">Sign Up</button>
-                        <button id="selected" type="button">Sign In</button>
+
+                        <button 
+                        id="signUp" 
+                        className={signValue === "0" ? "unselected" : "selected"} 
+                        onClick={() => {handleSelectedButton("signUp")}} 
+                        type="button">Sign Up
+                        </button>
+
+                        <button 
+                        id="signIn" 
+                        className={signValue === "0" ? "selected" : "unselected"} 
+                        onClick={() => {handleSelectedButton("signIn")}} 
+                        type="button">Sign In
+                        </button>
+
                     </div>
                     <div id="form-content">
                         <input type="text" name="nameN" id="nameI"/>
                         <input type="text" name="emailN" id="emailI"/>
-                        <button id="form-content-button" type="submit">Sign In</button>
+                        <button id="form-content-button" type="submit">{signValue === "0"? "Sign In" : "Sign Up"}</button>
                     </div>
                 </form>
             </main>
