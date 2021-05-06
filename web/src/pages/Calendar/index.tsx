08d0eca376f93131a204019319790f7e39ab8676
dhouglas <dhouglasbn@gmail.com>
2021-05-06 09:50:25 -0300

    commenting changeMonth buttons

--- web/src/pages/Calendar/index.tsx
+++ web/src/pages/Calendar/index.tsx
@@ -9,10 +9,12 @@ import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md"
 
 
 const Calendar = () => {
+    // states que vão ser utilizadas na página: year, month e message
     const [year, setYear] = useState(moment(new Date()).year())
     const [month, setMonth] = useState(moment(new Date()).month())
     const [message, setMessage] = useState("") 
 
+    // pegando o name que foi setado no localStorage durante o login
     const name = localStorage.getItem("name");
     
     // const email = localStorage.getItem("email");
@@ -34,9 +36,13 @@ const Calendar = () => {
     
     // alterando a mensagem de saudação de acordo com o horário do dia
     useEffect(() => {
+        // cada mensagem a ser mostrada
         const messages = ["Bom dia", "Boa tarde", "Boa noite"]
+
+        // consultando a hora do computador assim que a página carregar
         const hour = moment(new Date()).hour()
 
+        // lógica básica de programação para setar a mensagem
         if (hour >= 6 && hour <= 12) {
             setMessage(messages[0]);
         } else if (hour >= 12 && hour <= 18) {
@@ -48,21 +54,18 @@ const Calendar = () => {
 
     useEffect(() => {
 
-        /**
-         * const numbers = [1, 2, 3, 4, 5];
-         * const listItems = numbers.map((number) =>
-         * <li>{number}</li>
-         * ); 
-         */
-
-        const numbers = []
+        // criando uma array numbers
+        const numbers = [];
+        
+        // inserindo 35 números dessa array
         for (let index = 0; index < 35; index++) {
             numbers.push(index)
         }
         
+        // percorrendo cada item da array e atribuindo uma h3 para cada item a days
         const days = numbers.map(number => <h3 key={number} id={String(number)}>{number}</h3>)
         
-
+        // renderizando days na div "days"
         ReactDOM.render( days, document.getElementById("days"));
 
         
@@ -79,6 +82,9 @@ const Calendar = () => {
     }
     
     function handleAddMonth() {
+
+        // uma simples lógica para reduzir o mês ao clicar na seta
+        // se estivermos em janeiro o programa passa para o ano passado e vai para dezembero
         if(month === 11) {
             setYear(year + 1);
             setMonth(0);
