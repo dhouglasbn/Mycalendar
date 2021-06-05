909f038236bcd3903fe56208aa7f7899f049ac2c
dhouglas <dhouglasbn@gmail.com>
2021-06-05 12:03:22 -0300

    using ref concept

--- web/src/Components/Input.tsx
+++ web/src/Components/Input.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 // useField para conectar o input com o unform
 import { useField } from "@unform/core";
 
-export default function Input() {
+interface Props {
+    name: string
+}
+
+type InputProps = JSX.IntrinsicElements["input"] & Props
+
+export default function Input({ name, ...rest }: InputProps) {
+    
+    // referência do meu input que vai ser alterada na prop ref do input
+    const inputRef = useRef(null) 
+    const { fieldName, registerField } = useField(name)
+
     return (
-        <input />
+        // aqui a input ref passa a ser meu input, e com isso posso acessar todas as propriedades desse componente
+        <input ref={inputRef} {...rest} />
     )
 }
