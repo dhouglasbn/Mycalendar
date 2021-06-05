6b6629654c8f79764408cbc95c83b3901734404a
dhouglas <dhouglasbn@gmail.com>
2021-06-05 11:52:41 -0300

    working at my input with unform

---
+++ web/src/Components/Input.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+// useField para conectar o input com o unform
+import { useField } from "@unform/core";
+
+export default function Input() {
+    return (
+        <input />
+    )
+}
